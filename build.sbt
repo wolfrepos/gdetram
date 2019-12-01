96b840294aa9ca3682599cdf1f8f30d020b95c87
oybekbek <aybek_hashimov@mail.ru>
2019-12-01 21:34:40 +0500

    hello cats

--- build.sbt
+++ build.sbt
@@ -4,5 +4,8 @@ name          := "gdetram"
 organization  := "io.github.oybek"
 version       := "1.0"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core"   % "2.0.0",
+  "org.typelevel" %% "cats-effect" % "2.0.0"
+)
 
--- src/main/scala/io/github/oybek/gdetram/Main.scala
+++ src/main/scala/io/github/oybek/gdetram/Main.scala
@@ -1,5 +1,11 @@
 package io.github.oybek.gdetram
 
-object Main extends App {
-  println("Hello, World!")
+import cats.effect.IOApp
+import cats.effect.{ExitCode, IO}
+
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- IO { println("hello, cats!") }
+    } yield ExitCode.Success
 }
