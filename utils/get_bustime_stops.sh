cbdceebbc8135b3d56ba6b3c0c7d89cdc386513f
a-khakimov <techlinked@yandex.ru>
2019-12-20 23:26:17 +0500

    Add first version of script

---
+++ utils/get_bustime_stops.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Format
+# insert into stop values ('1168',   '1-й км',      'Пионерскую',           56.863213, 60.61197,  'tram'  );
+# insert into stop values ('962563', 'Ярославская', 'ст. Коммунистическая', 56.9129,   60.598927, 'troll' );
+# insert into stop values ('36222',  'Индустрии',   'Ильича',               56.899097, 60.59874,  'bus'   );
+# link: https://www.bustime.ru/ekaterinburg/stop/id/98120/
+
+
+LINK="https://www.bustime.ru"
+CITY="ekaterinburg"
+
+function using
+{
+	echo "HELP"
+	echo "Script for getting stop information from bustime"
+}
+
+
+function get_general_stop
+{
+	local curl_message="$1"
+	local stop=$(echo "$curl_message" | grep "stop_name" | sed "s/.*stop_name = \"//;s/\".*//")
+	echo -n "$stop"
+}
+
+function get_second_stop
+{
+	local curl_message="$1"
+	local stop=$(echo "$curl_message" | grep "fa-arrow-right" | sed -e "s/.*fa-arrow-right\"><\/i> //;s/<\/.*//")
+	echo -n "$stop"
+}
+
+function get_gps
+{
+	local curl_message="$1"
+	local x=$(echo "$curl_message" | grep US_CITY_POINT_X | sed "s/.*= //;s/;//")
+	local y=$(echo "$curl_message" | grep US_CITY_POINT_Y | sed "s/.*= //;s/;//")
+	echo -n "$x, $y"
+}
+
+function _delay
+{
+	local id=$1
+	if [[ $((id % 10)) -eq 0 ]]; then
+		sleep 1
+	fi
+}
+
+function main
+{
+	local id=35000 # for example
+	local max_id=35100
+	while [[ ! $id -gt $max_id  ]]; do
+		id=$((id + 1))
+		local curl_message=$(curl "$LINK/$CITY/stop/id/$id/" 2>/dev/null)
+		local general_stop=$(get_general_stop "$curl_message")
+		local second_stop=$(get_second_stop "$curl_message")
+		local gps=$(get_gps "$curl_message")
+	        if [[ -z "$general_stop" ]]; then
+			continue
+	        fi
+		if [[ -z "$second_stop" ]]; then
+			continue
+		fi
+		if [[ -z "$gps" ]]; then
+			continue
+		fi
+		echo "insert into stop values ($id, '$general_stop', '$second_stop', $gps)"
+		_delay $id
+	done
+
+}
+
+
+# run
+main
+
