40938a3350b6ed40d9dea687d5193cfc283b07f4
oybek <hashimov@tochka.com>
2020-05-30 23:54:00 +0500

    added sonar scan

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+
 ThisBuild / version := "0.1"
 ThisBuild / organization := "io.github.oybek"
 
@@ -11,4 +12,5 @@ lazy val plato = ProjectRef(
 lazy val gdetram = (project in file("."))
   .settings(name := "gdetram")
   .settings(libraryDependencies ++= Dependencies.common)
+  .settings(sonarProperties := Sonar.properties)
   .dependsOn(plato)
---
+++ project/Sonar.scala
@@ -0,0 +1,14 @@
+
+object Sonar {
+  val properties = Map(
+    "sonar.host.url" -> "https://sonarcloud.io",
+    "sonar.organization" -> "oybek",
+    "sonar.projectName" -> "gdetram",
+    "sonar.projectKey" -> "oybek_gdetram",
+    "sonar.sources" -> "src/main/scala",
+    "sonar.tests" -> "src/test/scala",
+    "sonar.sourceEncoding" -> "UTF-8",
+    "sonar.scala.scoverage.reportPath" -> "target/scala-2.12/scoverage-report/scoverage.xml",
+    "sonar.scala.coverage.reportPaths" -> "target/scala-2.12/scoverage-report/scoverage.xml",
+  )
+}
